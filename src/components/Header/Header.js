b8d056a6fc001b22275f191177c59ab91054761d
ndt-dinhtu <ndt.290303060703@gmail.com>
2026-04-01 14:37:20 +0700

    Thêm tính năng đăng xuất: cập nhật Header để xử lý đăng xuất người dùng, cải thiện quản lý trạng thái xác thực và thông báo cho người dùng.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,18 +3,33 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { NavLink, useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { LogOut } from "../../service/apiService";
+import { toast } from "sonner";
+import { doLogOut } from "../../redux/action/userAction";
 const Header = () => {
   const { isAuthenticated, account } = useSelector((state) => state.user);
 
   const navigate = useNavigate();
-
+  const dispatch = useDispatch();
   const handleLogin = () => {
     navigate("/login");
   };
 
   const handleRegister = () => {
     navigate("/register");
+  };
+
+  const handleLogout = async () => {
+    const rs = await LogOut(account.email, account.refresh_token);
+
+    if (rs && rs.EC === 0) {
+      toast.success(rs.EM);
+      dispatch(doLogOut());
+      navigate("/login");
+    } else {
+      toast.error(rs.EM);
+    }
   };
 
   return (
@@ -41,7 +56,9 @@ const Header = () => {
               {isAuthenticated ? (
                 <NavDropdown title="Setting" id="basic-nav-dropdown">
                   <NavDropdown.Item>Profile</NavDropdown.Item>
-                  <NavDropdown.Item>Log out</NavDropdown.Item>
+                  <NavDropdown.Item onClick={() => handleLogout()}>
+                    Log out
+                  </NavDropdown.Item>
                 </NavDropdown>
               ) : (
                 <>
--- src/redux/reducer/useReducer.js
+++ src/redux/reducer/useReducer.js
@@ -1,12 +1,12 @@
-import { INCREMENT, DECREMENT } from "../action/counterAction";
-import { FETCH_USER_LOGIN_SUCCESS } from "../action/userAction";
+import { FETCH_USER_LOGIN_SUCCESS, USER_DO_LOGOUT } from "../action/userAction";
 const INITIAL_STATE = {
   account: {
     username: "",
     access_token: "",
-    refersh_token: "",
+    refresh_token: "",
     image: "",
     role: "",
+    email: "",
   },
   isAuthenticated: false,
 };
@@ -18,17 +18,26 @@ const userReducer = (state = INITIAL_STATE, action) => {
         account: {
           username: action?.payload?.DT?.username,
           access_token: action?.payload?.DT?.access_token,
-          refersh_token: action?.payload?.DT?.refersh_token,
+          refresh_token: action?.payload?.DT?.refresh_token,
           image: action?.payload?.DT?.image,
           role: action?.payload?.DT?.role,
+          email: action?.payload?.DT?.email,
         },
         isAuthenticated: true,
       };
 
-    case DECREMENT:
+    case USER_DO_LOGOUT:
       return {
         ...state,
-        count: state.count - 1,
+        account: {
+          username: "",
+          access_token: "",
+          refresh_token: "",
+          image: "",
+          role: "",
+          email: "",
+        },
+        isAuthenticated: false,
       };
     default:
       return state;
